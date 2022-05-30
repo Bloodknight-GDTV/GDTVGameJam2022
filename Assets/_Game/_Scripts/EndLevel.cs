9157b799092bad2fc0aa38fc00cc63df2f9162d6
Bloodknight <blood.bks@gmail.com>
2022-05-30 21:22:33 +0100

    Added end level teleporter

---
+++ Assets/_Game/_Scripts/EndLevel.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+
+    PlayerMovement player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger triggered by {other};");
+        ReloadScene();
+    }
+
+    private void ReloadScene()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+        //GetComponent<Movement>().enabled = true;
+    }
+
+    // private void LoadNextScene()
+    // {
+    //     int currentScene = SceneManager.GetActiveScene().buildIndex;
+    //     int nextScene = ++currentScene;
+    //     if (nextScene == SceneManager.sceneCountInBuildSettings)
+    //     {
+    //         nextScene = 0;
+    //     }
+    //     SceneManager.LoadScene(nextScene);
+    //     //GetComponent<Movement>().enabled = true;
+    // }
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+
+    // }
+}
